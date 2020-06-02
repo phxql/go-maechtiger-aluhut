dba9c7cc8f04b1431eb2d1da37375bb83a191774
Moritz Kammerer <kammerer.moritz@gmail.com>
2020-06-02 14:04:11 +0200

    Set ShouldEndSession to allow reprompts

--- main.go
+++ main.go
@@ -50,14 +50,18 @@ func intentDispatchHandler(request *alexa.IntentRequest, response *alexa.Respons
 	switch request.Intent.Name {
 	case "ConspiracyIntent":
 		handleConspiracyIntent(request, response)
+	case "AMAZON.YesIntent":
+		handleConspiracyIntent(request, response)
+
 	case "AMAZON.HelpIntent":
 		handleHelpIntent(request, response)
+
 	case "AMAZON.StopIntent":
-		handleStopOrCancelIntent(request, response)
+		handleStopIntent(request, response)
 	case "AMAZON.CancelIntent":
-		handleStopOrCancelIntent(request, response)
-	case "AMAZON.YesIntent":
-		handleYesIntent(request, response)
+		handleStopIntent(request, response)
+	case "AMAZON.NoIntent":
+		handleStopIntent(request, response)
 	default:
 		log.Println("Unknown intent!", request.Intent.Name)
 	}
@@ -68,31 +72,44 @@ const welcome = "Willkommen beim mächtigen Aluhut. Du kannst dir die absoluten 
 const goodbye = "Auf Wiedersehen und bleib skeptisch!"
 const moreFacts = "Möchtest du noch eine Wahrheit hören?"
 
-func handleYesIntent(request *alexa.IntentRequest, responseEnvelope *alexa.ResponseEnvelope) {
-	handleConspiracyIntent(request, responseEnvelope)
-}
+// Used to set a pointer on it
+var aTrue = true
+var aFalse = false
 
 func handleConspiracyIntent(request *alexa.IntentRequest, responseEnvelope *alexa.ResponseEnvelope) {
+	log.Println("handleConspiracyIntent()")
+
 	randomFact := facts.RandomFact()
 
 	responseEnvelope.Response.SetOutputSpeech(randomFact + " " + moreFacts)
 	responseEnvelope.Response.SetReprompt(moreFacts)
+	responseEnvelope.Response.ShouldEndSession = &aFalse
 }
 
-func handleStopOrCancelIntent(request *alexa.IntentRequest, responseEnvelope *alexa.ResponseEnvelope) {
+func handleStopIntent(request *alexa.IntentRequest, responseEnvelope *alexa.ResponseEnvelope) {
+	log.Println("handleStopIntent()")
+
 	responseEnvelope.Response.SetOutputSpeech(goodbye)
+	responseEnvelope.Response.ShouldEndSession = &aTrue
 }
 
 func handleHelpIntent(request *alexa.IntentRequest, responseEnvelope *alexa.ResponseEnvelope) {
+	log.Println("handleHelpIntent()")
+
 	responseEnvelope.Response.SetOutputSpeech(welcomeReprompt)
 	responseEnvelope.Response.SetReprompt(welcomeReprompt)
+	responseEnvelope.Response.ShouldEndSession = &aFalse
 }
 
 func launchRequestHandler(request *alexa.LaunchRequest, responseEnvelope *alexa.ResponseEnvelope) {
+	log.Println("launchRequestHandler()")
 
 	responseEnvelope.Response.SetOutputSpeech(welcome)
 	responseEnvelope.Response.SetReprompt(welcomeReprompt)
+	responseEnvelope.Response.ShouldEndSession = &aFalse
 }
 
 func sessionEndedRequestHandler(request *alexa.SessionEndedRequest, responseEnvelope *alexa.ResponseEnvelope) {
+	log.Println("sessionEndedRequestHandler()")
+	responseEnvelope.Response.ShouldEndSession = &aTrue
 }
